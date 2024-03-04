c44683e344a9e3518121e4b4e4daa82f9de46eda
Aleksei Korotkikh <lexa.36484@gmail.com>
2024-03-04 11:58:39 +0500

    добавил логирование перевода денежных средств между аккаунтами

--- src/main/java/org/example/bankservice/service/AccountService.java
+++ src/main/java/org/example/bankservice/service/AccountService.java
@@ -4,6 +4,7 @@ import jakarta.transaction.Transactional;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.example.bankservice.domain.Account;
 import org.example.bankservice.exception.TransferMoneyException;
@@ -18,6 +19,7 @@ import java.util.List;
 import java.util.concurrent.locks.ReentrantLock;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AccountService {
@@ -28,6 +30,9 @@ public class AccountService {
 
     ReentrantLock lock = new ReentrantLock();
 
+    /**
+     * Увеличение баланса на всех аккаунтов на 5%, но не более чем на 207% от начальной величины
+     */
     @Transactional
     public void increaseBalanceForAllAccounts() {
         List<Account> accounts = accountRepository.findAll();
@@ -48,10 +53,16 @@ public class AccountService {
     @Transactional
     public void transferMoney(double amount, long recipientUserId) {
         lock.lock();
+
         try {
             val currentUser = userService.getCurrentUser();
+            log.info("перевод денежных средств пользователя - {}, пользователю - {}, сумма - {}",
+                    currentUser.getId(),
+                    recipientUserId,
+                    amount);
 
             var account = accountRepository.findByUser(currentUser).orElseThrow(UserAccountNotFoundException::new);
+            log.info("баланс отправителя до перевода - {}", account.getBalance());
 
             if (account.getBalance() < amount) {
                 throw new TransferMoneyException("Недостаточно средств для перевода");
@@ -60,9 +71,12 @@ public class AccountService {
             var recipientUserAccount = userRepository.findByIdFetchAccount(recipientUserId)
                     .orElseThrow(UserEmailNotFoundException::new)
                     .getAccount();
+            log.info("баланс получателя до перевода - {}", recipientUserAccount.getBalance());
 
             account.setBalance(DoubleUtil.formatDouble(account.getBalance() - amount));
+            log.info("баланс отправителя после перевода - {}", account.getBalance());
             recipientUserAccount.setBalance(DoubleUtil.formatDouble(Double.sum(recipientUserAccount.getBalance(), amount)));
+            log.info("баланс получателя после перевода - {}", recipientUserAccount.getBalance());
         } finally {
             lock.unlock();
         }
